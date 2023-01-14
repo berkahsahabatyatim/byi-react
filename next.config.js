b0d8dc42f2f11d1d0fb541c015e19be7ee7b0d69
nashihu <ahmad.n.id@gmail.com>
2023-01-14 21:02:04 +0700

    ref: dokum page

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     distDir: 'build',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+                port: '',
+                pathname: '/nashihu/**',
+            },
+        ],
+    },
 }
--- pages/dokumentasi/index.js
+++ pages/dokumentasi/index.js
@@ -3,43 +3,37 @@ import Footer from '../../src/component/Footer'
 import Header from '../../src/component/Header'
 import Masthead from '../../src/component/Masthead'
 import axios from "axios"
-import { useLoaderData } from "react-router-dom"
-import Helmets from "../../src/component/Helmet"
+import Image from "next/image"
+import Head from "next/head"
 
-export default function sipp() {
-    return (<div>Hello</div>)
-}
-
-export async function dokumLoader() {
+export async function getStaticProps() {
     try {
         var res = await axios.get("https://api.github.com/repos/nashihu/production_stuff/contents/bsy_images")
-        var resp = res.data;
-        resp.sort(function (a, b) {
+        var data = res.data;
+        data.sort(function (a, b) {
             return b.download_url.localeCompare(a.download_url);
         });
-        return { data: resp };
+        return { props: { data } };
     } catch (e) {
-        console.log("kena err ", e)
-        return { data: [] };
+        console.log("kena errr ", e)
+        return { props: { data: [] } };
     }
 
 }
 
 export const dokum = "/dokumentasi"
 
-export function Dokum() {
-    var { data } = useLoaderData()
-    console.log(data);
+export default function Dokum({ data }) {
     return (<div>
         <DokumHelmet />
         <Header />
         <Masthead buttonLabel="Lihat Album" target="#album" />
-        <div class="container page-top mt-5">
-            <div class="row" id="album">
+        <div className="container page-top mt-5">
+            <div className="row" id="album">
                 {data.map((a) => (
-                    <div class="col-lg-3 col-md-4 col-xs-6 thumb mb-4">
+                    <div className="col-lg-3 col-md-4 col-xs-6 thumb mb-4">
                         <a data-fancybox="gallery" href={a.download_url} rel="ligthbox" >
-                            <img src={a.download_url} class="img-fluid zoom loadingImg" alt="" loading="lazy" height="300" width="300" />
+                            <Image src={a.download_url} className="img-fluid zoom loadingImg" alt="" loading="lazy" height="300" width="300" />
                         </a></div>))}
 
             </div>
@@ -49,11 +43,12 @@ export function Dokum() {
 }
 
 function DokumHelmet() {
-    return (<Helmets>
+    return (<Head>
+        <title>Dokumentasi BYI</title>
         <meta charSet="utf-8" />
         <meta property="og:title" content="Dokumentasi" />
         <meta property="og:description" content="Dokumentasi Yayasan BYI" />
         <meta property="og:image"
             content="https://raw.githubusercontent.com/nashihu/production_stuff/master/bsy_images/2020-01-24%2018.19.45.jpeg" />
-    </Helmets>)
+    </Head>)
 }
