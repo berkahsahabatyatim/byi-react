268a616b4a4411e7877ca9192b53a26475a5c1f7
nashihu <ahmad.n.id@gmail.com>
2023-01-15 16:06:20 +0700

    fix: dependencies

--- pages/kajian/index.js
+++ pages/kajian/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import { useLoaderData } from 'react-router-dom';
 import axios from "axios"
 import Footer from '../../src/component/Footer'
 import Header from '../../src/component/Header'
