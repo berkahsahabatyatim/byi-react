f6db82ce4fd600897e2546cf81cc40911cfdf670
nashihu <ahmad.n.id@gmail.com>
2023-01-19 10:42:31 +0700

    edit: visi misi

--- pages/about/visi-misi/index.js
+++ pages/about/visi-misi/index.js
@@ -16,13 +16,12 @@ export default function Vision() {
                 <div className="entry-content">
                     <p><strong>Visi Misi Yayasan Berkah Yatim Indonesia</strong></p>
                     <p><strong>Visi</strong></p>
-                    <p className="has-text-align-center">Mewujudkan pondok tahfidz yatim dan dhuafa di Indonesia</p>
+                    <p className="has-text-align-center">Menjadi lembaga filantropi nasional untuk mewujudkan pondok tahfidz yatim dan dhuafa di Indonesia</p>
                     <p><strong>Misi</strong></p>
                     <p>Membangun lembaga yang amanah dan profesional sebagai fasilitator kaum muslimin untuk membentuk generasi qurani menjadi hamba-hamba Allah yang bermanfaat bagi banyak orang</p>
                     <p>Membangun dan membina pondok tahfidz yatim & dhuafa berbasis kemandirian ekonomi di Indonesia</p>
                     <p>Mencetak sejuta penghafal Al-Qur'an</p>
                     <p>Memberdayakan ekonomi masyarakat</p>
-                    <p>Menjadi Lembaga Filantropi untuk Pemberdayaan Ummat</p>
                     <br />
                 </div>
             </div>
