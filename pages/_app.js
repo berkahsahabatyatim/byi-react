0b0bc2a8b1022d847a1356e569388c846b9c9c10
nashihu <ahmad.n.id@gmail.com>
2023-03-27 09:47:03 +0700

    google tag

--- pages/_app.js
+++ pages/_app.js
@@ -24,6 +24,16 @@ export default function MyApp({ Component, pageProps }) {
   return (<>
     <Script src="https://code.jquery.com/jquery-3.1.1.min.js" strategy='beforeInteractive' />
     <Script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" />
+    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-PLDNBMKCX5"></Script>
+    <Script id="google-tag-manager" strategy="afterInteractive">
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+      
+        gtag('config', 'G-PLDNBMKCX5');
+      `}
+    </Script>
     <ErrorBoundary> <Component {...pageProps} /></ErrorBoundary>
   </>
   )
